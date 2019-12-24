1d23a674c42edd651bdf198408e092c2f4a520da
Innocent1312 <anthonydev33@gmail.com>
2019-12-24 10:11:35 +0200

    Updates

---
+++ precache-manifest.29af258e8c9b4e1a71d2f804e70524f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92db0865fa166e04cb960eb667eaa73",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "92913b76bac44dbd5e7d",
+    "url": "/react_dynamic-list-of-posts/static/css/main.827706a3.chunk.css"
+  },
+  {
+    "revision": "d6b5931bbac40714b0ae",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a1ae89e9.chunk.js"
+  },
+  {
+    "revision": "92913b76bac44dbd5e7d",
+    "url": "/react_dynamic-list-of-posts/static/js/main.e98d8409.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
